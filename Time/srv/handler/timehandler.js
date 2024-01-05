1a5fff5a268748e4815d31415bfefe9401d72caa
Somasree Majumder <smajumder@deloitte.com>
2024-01-05 08:37:29 +0000

    change

--- Time/srv/handler/timehandler.js
+++ Time/srv/handler/timehandler.js
@@ -30,78 +30,18 @@ const usersLeaveReport = async (req) => {                                      /
   if (EmpInfo) {
     const usersArray = EmpInfo.map(obj => obj.userId);                     //the array stores the usersId
     console.log(usersArray);
-    const query = SELECT.from("EmployeeTime", ["externalCode", "timeType", "approvalStatus", "userId", "startDate", "endDate"])
+    const query = SELECT.from("EmployeeTime", ["externalCode", "timeType", "userId", "startDate", "endDate"])
       .where(`userId IN`, usersArray)
-    //`and approvalStatus IN`, ['APPROVED', 'PENDING'])
     info = await txecto.send({ method: "READ", query });
     return info;
   }
 }
-
-const ectimeEmployees = async (req) => {
-  try {
-    var { $top, $filter } = req._query;
-    console.log('query', req._query);
-    //req.user.id='100135'
-    //$skip = $skip ? $skip : 0;
-    $top = $top ? $top : 5;
-    let filters = $filter ? await enrichFilterDetails($filter.toString()) : "";
-    //let count = 0;
-    // let flattenedData = [];
-    
-      let sURL = filters
-
-        ? `/EmployeeTime?$format=json
-            &$select=userId,startDate,endDate,approvalStatus,timeType,
-            &$top=${$top}&$filter='${filters}'`
-
-        : `/EmployeeTime?$format=json
-          &$select=userId,startDate,endDate,approvalStatus,timeType,
-          &$top=${$top}`;
-      //const txecto = ECTimeOff.transaction(req);
-
-      const leaveReports = await ECTimeOff.send({ method: "GET", query: sURL });
-      
-
-      if (Array.isArray(leaveReports) && leaveReports.length) {
-
-        // leaveReports.forEach((item) => {
-        //   flattenedData.push({
-        //     userId: item.userId,
-        //     timeType: item.timeType,
-        //     startDate: item.startDate,
-        //     endDate: item.endDate, 
-        //     approvalStatus: item.approvalStatus
-        //   });
-        // });
-    console.log(leaveReports);
-    return leaveReports;
-        
-
-      } else {
-        log.info(`Couldn't find any pending records`);
-      }
-    
-  
-    
-  } catch (e) {
-    throw new Error(e);
-  }
-};
-
-const enrichFilterDetails = async (sFilter) => {
-  console.log("sFilter", sFilter);
-  sFilter = sFilter.replace("userId", "userId");
-  console.log("After replacement ", sFilter);
-  return sFilter;
-};
 
 
 
 
 module.exports = {
   getUsersECtime,
-  usersLeaveReport,
-  ectimeEmployees
+  usersLeaveReport
 }
 
